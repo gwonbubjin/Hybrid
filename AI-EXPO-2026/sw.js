0c74272ac6ed7467ba7ebca4d9e99d5309a92887
gwonbubjin <102206238+gwonbubjin@users.noreply.github.com>
2025-11-21 13:47:33 +0900

    Add files via upload

---
+++ AI-EXPO-2026/sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'ai-expo-v1';
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './bg1.jpg',
+  './bg2.jpg',
+  './bg3.jpg',
+  './poster.jpg'
+];
+
